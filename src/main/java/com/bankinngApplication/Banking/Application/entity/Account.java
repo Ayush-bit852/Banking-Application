3ee1827ae6bf76c4701c00f5bc5e1428ee374263
Ayush Sharma <77086486+Ayush-bit852@users.noreply.github.com>
2025-04-08 12:34:27 +0530

    Updated

---
+++ src/main/java/com/bankinngApplication/Banking/Application/entity/Account.java
@@ -0,0 +1,22 @@
+package com.bankinngApplication.Banking.Application.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String accountNumber;
+    private double balance;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
+    private List<Transaction> transactions = new ArrayList<>();
+}
